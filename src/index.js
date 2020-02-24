f768bb91f3b2b3bc7e088ee91ee5e14d4af10032
Islombek Khasanov <islombekhasanov@gmail.com>
2020-02-24 16:02:01 +0500

    feat: test and finish Typical Arrays Problems task

---
+++ src/index.js
@@ -0,0 +1,14 @@
+exports.min = function min(array) {
+    if (!array || !array.length) return 0
+    return Math.min(...array)
+}
+
+exports.max = function max(array) {
+    if (!array || !array.length) return 0
+    return Math.max(...array);
+}
+
+exports.avg = function avg(array) {
+    if (!array || !array.length) return 0
+    return array.reduce((accumulator, currentValue) => accumulator + currentValue) / array.length
+}
